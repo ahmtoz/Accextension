2d23a1a832d342735cb75136bd5aa64454ab2270
Ahmet Öz <Ahmet Z>
2025-12-06 11:38:58 +0300

    home.css approach added to centralized section styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import Navbar from './../../components/Navbar.jsx';
 import HeroSection from './sections/HeroSection.jsx';
 import InfoSection from './sections/InfoSection.jsx';
 import FeaturesSection from './sections/FeaturesSection.jsx'
+import './../../styles/home.css';
 
 function Home() {
   return (
