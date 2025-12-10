1b1526b0c0c7f4ec5fdeae78626de9005669a92c
Ahmet Öz <Ahmet Z>
2025-12-10 17:02:04 +0300

    mobile hamburger menu added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,35 @@
+import { useState } from 'react';
 import '../styles/navbar.css';
 
 function Navbar () {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav>
       <div className='nav-logo'>
         <a href="#">
-            <img src="" alt="" />
-            <span>AccEase</span>
+          <img src="" alt="AccEase Logo" />
+          <span>AccEase</span>
         </a>
       </div>
-      <div className='nav-links'>
+      
+      <div className='hamburger' onClick={toggleMenu}>
+        <div className='bar'></div>
+        <div className='bar'></div>
+        <div className='bar'></div>
+      </div>
+
+      <div className={`nav-links ${isOpen ? 'open' : ''}`}>
         <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Add - Ons</a></li>
-            <li><a href="#">User Guide</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Contact</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Add - Ons</a></li>
+          <li><a href="#">User Guide</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Contact</a></li>
         </ul>
       </div>
     </nav>
