5ecf27d2a523243bee2d5788868d0f1fa8d34349
Ahmet Öz <Ahmet Z>
2025-12-12 21:05:47 +0300

    hero section img and font styles added with font-family

--- src/pages/Home/sections/HeroSection.jsx
+++ src/pages/Home/sections/HeroSection.jsx
@@ -1,20 +1,20 @@
 import '../../../styles/HeroSection.css';
-
+import heroImage from '../../../assets/images/hero-image.png';
 function HeroSection() {
     return(
         <section>
             <div className="container">
                 <div className="hero-information">
-                    <h1>Unlock the Web's Full Potential</h1>
                     <h4>“Making the internet comfortable for everyone.”</h4>
+                    <h1>Unlock the Web's Full Potential</h1>
                     <p>AccessEase provides add-ons for a personalized experience. Overcomes color blindness, dyslexia, ADHD with smart, seamless integration.</p>
                     <div className="cta-buttons">
-                        <button className="get-started">Get Started Free</button>
+                        <button className="get-started">Get Started Free </button>
                         <button className="video">Watch the Video</button>
                     </div>
                 </div>
                 <div className="hero-image">
-                    <img src="hero-image.png" alt="Hero Image" />
+                    <img src={heroImage} alt="Hero Image" />
                 </div>
             </div>
         </section>
