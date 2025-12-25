858d92bd67c6b5ecc0a39cf905dc851d128374b0
tsndogan <dogantsn61@gmail.com>
2025-12-25 21:19:50 +0300

    Manifest file added and working but there is some missing parts.

---
+++ src/Popup.jsx
@@ -0,0 +1,13 @@
+function Popup() {
+  return (
+    <div style={{ padding: 10, width: 200 }}>
+      <h4>BU BİR POPUP</h4>
+      <label>
+        <input type="checkbox" />
+        Aktif Et
+      </label>
+    </div>
+  );
+}
+
+export default Popup;
