2589d2aa3f4b238a80daad4bbca9228ea685c04c
Ahmet Öz <Ahmet Z>
2025-12-25 23:19:26 +0300

    popup style added

--- src/Popup.jsx
+++ src/Popup.jsx
@@ -1,11 +1,32 @@
+import '../src/styles/popup.css';
+
 function Popup() {
   return (
-    <div style={{ padding: 10, width: 200 }}>
-      <h4>BU BİR POPUP</h4>
-      <label>
-        <input type="checkbox" />
-        Aktif Et
-      </label>
+    <div className="popup-container">
+      <h3>Easeo</h3>
+      <div className='popup-options'>
+        <div className="option">
+          <div className="option-info">
+            <img src="" alt="IMG1" />
+            <span>Color Blindess</span>
+          </div>
+          <button type="button">Apply</button>
+        </div>
+        <div className="option">
+          <div className="option-info">
+            <img src="" alt="IMG2" />
+            <span>Dyslexia</span>
+          </div>
+          <button type="button">Apply</button>
+        </div>
+        <div className="option">
+          <div className="option-info">
+            <img src="" alt="IMG2" />
+            <span>Keyboard Nav</span>
+          </div>
+          <button type="button">Apply</button>
+        </div>
+      </div>
     </div>
   );
 }
