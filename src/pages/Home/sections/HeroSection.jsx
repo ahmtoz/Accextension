58f4173709da6fb8652e4bee4ad68e0a1cd8612a
Ahmet Öz <Ahmet Z>
2025-12-29 00:05:30 +0300

    hero section layout and style updated

--- src/pages/Home/sections/HeroSection.jsx
+++ src/pages/Home/sections/HeroSection.jsx
@@ -7,13 +7,16 @@ function HeroSection() {
                 <div className="hero-information">
                     <h4>“Making the internet comfortable for everyone.”</h4>
                     <h1>Unlock the Web's Full Potential</h1>
+                    <div className="hero-image hero-image-mobile">
+                        <img src={heroImage} alt="Hero Image" />
+                    </div>
                     <p>AccessEase provides add-ons for a personalized experience. Overcomes color blindness, dyslexia, ADHD with smart, seamless integration.</p>
                     <div className="cta-buttons">
                         <button className="get-started">Get Started Free </button>
                         <button className="video">Watch the Video</button>
                     </div>
                 </div>
-                <div className="hero-image">
+                <div className="hero-image hero-image-desktop">
                     <img src={heroImage} alt="Hero Image" />
                 </div>
             </div>
