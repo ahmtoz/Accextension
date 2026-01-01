e90fb36315992e0d242669821f5dff1969a3b1a9
Ahmet Öz <Ahmet Z>
2026-01-01 14:17:18 +0300

    quote section added

--- src/pages/About/sections/InfoSection.jsx
+++ src/pages/About/sections/InfoSection.jsx
@@ -1,8 +1,19 @@
+import "../styles/infoSection.css";
+import AboutImage from "../../../assets/images/about-image.png";
+
 function InfoSection() {
     return (
-        <section className="info-section">
-            <h2>Our Story</h2>
-            <p>We are dedicated to providing the best service to our customers.</p>
+        <section>
+            <div className="about-info-section">
+                <h2>Who are we?</h2>
+                <div className="about-info-container">
+                    <img src={AboutImage} alt="" />
+                    <p>Easeo is an essential browser add-on dedicated to creating a truly inclusive internet experience for everyone. Easeo is an essential browser add-on dedicated to creating a truly inclusive internet experience for everyone. Easeo is an essential browser add-on dedicated to creating a truly inclusive internet experience for everyone.AccesEase is an essential browser add-on dedicated to creating a truly inclusive internet experience for everyone.</p>
+                </div>
+                <blockquote>
+                    "Our mission is to empower individuals with disabilities by providing them with the tools they need to navigate the digital world with ease and confidence."
+                </blockquote>
+            </div>
         </section>
     );
 }
