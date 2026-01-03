420e2a89bd9560cdb4864171af523d96127205b4
Ahmet Öz <Ahmet Z>
2026-01-03 14:36:10 +0300

    video section issue fixed

--- src/pages/Home/sections/VideoSection.jsx
+++ src/pages/Home/sections/VideoSection.jsx
@@ -1,4 +1,4 @@
-import "../styles/VideoSection.css";
+import "../styles/videoSection.css";
 
 function VideoSection() {
   return (
@@ -7,8 +7,6 @@ function VideoSection() {
             <h3>Explore Our Porducts</h3>
             <div className="video-container">
                 <iframe
-                width="1120"
-                height="630"
                 src="https://www.youtube.com/embed/SOa3vIg8In4?si=vAF4t8tC3qAufrA2"
                 title="YouTube video player"
                 frameborder="0"
