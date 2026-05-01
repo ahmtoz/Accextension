3be5e5b8393ab2e7d73459d1424d7258f7b6fde4
Ahmet Öz <ahmtoz@users.noreply.github.com>
2026-05-01 22:18:39 +0300

    accessibility fix

--- src/pages/home/sections/HeroSection.jsx
+++ src/pages/home/sections/HeroSection.jsx
@@ -4,7 +4,7 @@ import cta1 from '../../../assets/images/cta1.svg';
 import cta2 from '../../../assets/images/cta2.svg';
 
 function HeroSection() {
-    return(
+    return (
         <section id='hero'>
             <div className="container">
                 <div className="hero-information">
@@ -15,21 +15,21 @@ function HeroSection() {
                     </div>
                     <p>Accextension provides add-ons for a personalized experience. Overcomes color blindness, dyslexia, ADHD with smart, seamless integration.</p>
                     <div className="cta-buttons">
-                        <button 
-                        className="get-started-btn"
-                        onClick={() => {
-                            document.querySelector("#features-section")
-                            .scrollIntoView({behavior: "smooth"})
-                        }}
-                        >Get Started Free <img src={cta1} alt="CTA1" /></button>
-                        <button 
-                        className="video-btn"
-                        onClick={() => {
-                            document
-                            .getElementById("video-section")
-                            .scrollIntoView({ behavior: "smooth" });
-                        }}
-                        >Watch the Video <img src={cta2} alt="CTA2" /></button>
+                        <button
+                            className="get-started-btn"
+                            onClick={() => {
+                                document.querySelector("#features-section")
+                                    .scrollIntoView({ behavior: "smooth" })
+                            }}
+                        >Get Started Free <img src={cta1} alt="" aria-hidden="true" /></button>
+                        <button
+                            className="video-btn"
+                            onClick={() => {
+                                document
+                                    .getElementById("video-section")
+                                    .scrollIntoView({ behavior: "smooth" });
+                            }}
+                        >Watch the Video <img src={cta2} alt="" aria-hidden="true" /></button>
                     </div>
                 </div>
                 <div className="hero-image hero-image-desktop">
